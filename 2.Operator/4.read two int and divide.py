a49d7bd4d5e60d31e53b7baab29dab5caa5b2c0d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-03 20:17:29 +0600

    python codes

---
+++ 2.Operator/4.read two int and divide.py
@@ -0,0 +1,12 @@
+
+x = int(input("enter int: "))
+
+y = int(input("enter int: "))
+
+if(not y == 0):
+    result = x/y;
+
+    print("Result is: ", result)
+
+else:
+    print("Math Error!!! \nResult is infinity")
