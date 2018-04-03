fc990c5e83e5a1ec32141ec80d61852abd514dc5
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-03 22:24:34 +0600

    python codes

--- 3.Conditional Logic/2. read 3 num and display maximum.py
+++ 3.Conditional Logic/2. read 3 num and display maximum.py
@@ -4,3 +4,8 @@ b = int(input("enter int number: "))
 c = int(input("enter int number: "))
 
 if(a>b and a>c):
+    print("The Maximum is: ",a)
+elif (b>a and b>c):
+    print("The Maximum is: ",b)
+else:
+    print("The maximum is: ",c)
