e9f1b43a5a5436ec6b6e2a381a7f005f0d5a613f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-04 02:42:53 +0600

    python codes

--- 3.Conditional Logic/5.vowels and consonants.py
+++ 3.Conditional Logic/5.vowels and consonants.py
@@ -1,1 +1,7 @@
 
+char = str(input("entar a character: "))
+
+if(char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u' or char == 'A' or char == 'E' or char == 'I' or char == 'O' or char == 'U'):
+    print("Vowel")
+else:
+    print("Constant")
