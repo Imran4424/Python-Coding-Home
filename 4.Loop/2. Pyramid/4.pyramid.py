0b702ac4222b8fbcb634b897fa07e1cf63d91af0
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-04 20:38:17 +0600

    python codes

--- 4.Loop/2. Pyramid/4.pyramid.py
+++ 4.Loop/2. Pyramid/4.pyramid.py
@@ -8,8 +8,8 @@ Problem:
 '''
 
 for i in range(1, 6):
-    for j in range(i):
-        if(i % 2 == 0):
+    for j in range(1,i+1):
+        if(j % 2 == 0):
             print("0", end=' ')
         else:
             print("1", end=' ')
