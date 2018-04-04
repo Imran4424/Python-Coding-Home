afeca715326c4cb38dd8a034d66bc8e263d11456
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-04 20:37:45 +0600

    python codes

--- 4.Loop/2. Pyramid/4.pyramid.py
+++ 4.Loop/2. Pyramid/4.pyramid.py
@@ -6,3 +6,12 @@ Problem:
         1 0 1 0 1
 
 '''
+
+for i in range(1, 6):
+    for j in range(i):
+        if(i % 2 == 0):
+            print("0", end=' ')
+        else:
+            print("1", end=' ')
+
+    print()
