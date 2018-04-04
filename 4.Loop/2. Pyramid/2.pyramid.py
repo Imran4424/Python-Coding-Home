327b3ff5e207ba7ae76e5436011c57ee92fdd3ad
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-04 19:03:07 +0600

    python codes

--- 4.Loop/2. Pyramid/2.pyramid.py
+++ 4.Loop/2. Pyramid/2.pyramid.py
@@ -9,7 +9,7 @@ Problem:
 
 for i in range(1,4):
     for j in range(1,i+1):
-        print(i,end=' ')
+        print(i,end=' ')        # by editing end variable we can put any string or character in last
     
     print()
 
---
+++ 4.Loop/2. Pyramid/3. pyramid.py
@@ -0,0 +1,6 @@
+
+for i in range(20):
+    print('a',end='')
+
+
+print(2,3,4,sep='-',end='\n',file=sys.stdout,flush=False)
