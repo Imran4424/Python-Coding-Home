0c843b3ed8666d34317145930189c2e79edafdf6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-04 07:58:10 +0600

    python codes

---
+++ 4.Loop/1.series with while loop.py
@@ -0,0 +1,19 @@
+# 1+2+3+....+n
+
+n = int(input("enter an int: "))
+
+num = 1
+sum = 0
+
+while(num <= n):
+
+    sum += num
+    num = num+1
+
+
+print("The series summation is: ", sum)
+
+
+    
+
+
