0fbbc1a8cf02813033acc728a7ac70e3bacd5dc9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-04 20:24:09 +0600

    python codes

--- 4.Loop/2. Pyramid/3. pyramid.py
+++ 4.Loop/2. Pyramid/3. pyramid.py
@@ -1,3 +1,16 @@
 import time
+import sys
 
-print("I am a Programmer",flush = True)
+
+for i in range(20):
+    print("I am a Programmer",flush=True)
+    time.sleep(1.0)
+
+
+for n in range(0,10):
+    
+    print "-"
+
+    sys.stdout.flush()
+
+    time.sleep(1.0)
