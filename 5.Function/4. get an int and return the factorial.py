5b4f4b51051a548331ba99ab4f7c20447fc49e01
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-05 02:51:42 +0600

    python codes

---
+++ 5.Function/4. get an int and return the factorial.py
@@ -0,0 +1,13 @@
+
+def factorial(n):
+    if(n==1):
+        return 1
+
+    return n*factorial(n-1)
+
+
+n = int(input("enter an int: "))
+
+result = factorial(n)
+
+print("Factorial of that number is: ",result)
