e4cf4516227fbfba7609fde3c90488635cbdca45
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-04 18:14:20 +0600

    python codes

--- 4.Loop/2. Pyramid/1.pyramid.py
+++ 4.Loop/2. Pyramid/1.pyramid.py
@@ -3,15 +3,16 @@ problem:
 
     1
     12
-    123 
+    123
 
 '''
 
- for i in range(1,4):
+
+for i in range(1,4):
     for j in range(1,i+1):
         print(j,end='')
-        
-    
+
+    print()
 
 
 
