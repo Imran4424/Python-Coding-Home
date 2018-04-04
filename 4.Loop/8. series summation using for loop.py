898d06e161f70eb6afe6d1030d56c78ade10a59e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-04-04 17:41:59 +0600

    python codes

--- 4.Loop/8. series summation using for loop.py
+++ 4.Loop/8. series summation using for loop.py
@@ -1,4 +1,4 @@
-# 1^3+2^3+3^3+...+n^3
+# 1^1+2^2+3^3+...+n^n
 
 n = int(input("enter an int: "))
 
@@ -6,7 +6,7 @@ summation = 0
 
 for i in range(1, n+1):
 
-    summation += pow(i, 3)
+    summation += pow(i, i)
 
 
 print("The Summation is: ", summation)
